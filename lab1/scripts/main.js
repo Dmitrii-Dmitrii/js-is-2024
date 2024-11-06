43c05295f4cd5e5b3761ba0b748c2a1a64c345ed
Dmitrii Onshin <dmitrii.onshin@mail.ru>
2024-11-06 14:28:25 +0300

    start lab4

---
+++ lab1/scripts/main.js
@@ -0,0 +1,27 @@
+(function() {
+    window.addEventListener('load', () => {
+        const loadTime = performance.now();
+        const footer = document.querySelector('footer');
+        const loadInfo = document.createElement('p');
+        loadInfo.textContent = `Время загрузки страницы: ${loadTime.toFixed(2)} мс`;
+        footer.appendChild(loadInfo);
+    });
+})();
+
+document.querySelectorAll('nav ul li a').forEach(item => {
+    item.addEventListener('mouseover', () => {
+        item.style.color = '#005f99';
+    });
+    item.addEventListener('mouseout', () => {
+        item.style.color = '#432b1d';
+    });
+});
+
+const links = document.querySelectorAll('nav ul li a');
+const currentPage = window.location.href;
+
+links.forEach(link => {
+    if (link.href === currentPage) {
+        link.classList.add('active');
+    }
+});
